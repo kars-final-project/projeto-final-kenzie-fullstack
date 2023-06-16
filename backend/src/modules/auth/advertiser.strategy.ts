165065453e2818549c2082c2b0573c98ce050562
Juan <juan.ortiz96@gmail.com>
2023-06-16 12:51:04 -0300

    feat: added strategies for route protection

---
+++ backend/src/modules/auth/advertiser.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AdvertiserStrategy extends PassportStrategy(Strategy, 'advertiser') {
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
+  }
+
+  async validate(email: string, password: string) {
+    const user = await this.authService.validateUser(email, password);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    } else if (user.type !== 'ANUNCIANTE') {
+        throw new UnauthorizedException('Unauthorized access');
+    }
+
+    return user;
+  }
+}
--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { JwtStrategy } from './jwt.strategy';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -5,6 +6,7 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalStrategy } from './local.strategy';
+import { AdvertiserStrategy } from './advertiser.strategy';
 
 @Module({
   imports: [
@@ -16,6 +18,6 @@ import { LocalStrategy } from './local.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy, AdvertiserStrategy],
 })
 export class AuthModule {}
