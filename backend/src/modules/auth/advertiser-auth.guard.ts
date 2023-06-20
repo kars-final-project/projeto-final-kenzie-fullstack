93838b8bc9cdc602ad8131b0938e1dd22d3b5934
iarley1 <iarley96@gmail.com>
2023-06-20 13:11:20 -0300

    check user type

--- backend/src/modules/auth/advertiser-auth.guard.ts
+++ backend/src/modules/auth/advertiser-auth.guard.ts
@@ -1,5 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
-export class AdvertiserAuthGuard extends AuthGuard('advertiser') {}
+export class AdvertiserAuthGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean  {
+        const req = context.switchToHttp().getRequest()
+        const user = req.user
+
+        if(user.type !== "ANUNCIANTE"){
+            throw new UnauthorizedException('Unauthorized access');
+        }
+
+        return true
+    }
+}
